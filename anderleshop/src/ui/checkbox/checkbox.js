a84ee249d0ba670e5c8a4c0c1c9641ab4ef6d682
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-06-05 23:05:28 +0200

    Need to create slider.

--- anderleshop/src/ui/checkbox/checkbox.js
+++ anderleshop/src/ui/checkbox/checkbox.js
@@ -1,12 +1,15 @@
-
 import "./main.css";
 
 const Checkbox = ({ label, checked, onChange }) => {
     return (
-        <label>
+        <label className="checkbox-container">
             <input
+                className="checkbox-input"
                 type="checkbox"
+                checked={checked}
+                onChange={onChange}
             />
+            <span className="checkbox-custom"></span>
             {label}
         </label>
     );
